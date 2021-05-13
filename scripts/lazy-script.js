0d87de859097027f10226b124f179aea82fe415e
Carlo Nucera <carlo.nucera@protonmail.com>
2021-05-12 20:50:57 -0400

    Fix _signTypedData error in test suite

--- scripts/lazy-script.js
+++ scripts/lazy-script.js
@@ -1,5 +1,27 @@
 const hre = require("hardhat");
 
+const Mint1155DataType = {
+  Part: [
+    { name: "account", type: "address" },
+    { name: "value", type: "uint96" },
+  ],
+  Mint1155Data: [
+    { name: "tokenId", type: "uint" },
+    { name: "uri", type: "string" },
+    { name: "supply", type: "uint" },
+    { name: "creators", type: "Part[]" },
+    { name: "royalties", type: "Part[]" },
+    { name: "signatures", type: "bytes[]" },
+  ],
+};
+
+const domain = {
+  name: "Ether Mail",
+  version: "1",
+  chainId: 1,
+  verifyingContract: "0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC",
+};
+
 async function main() {
   const [owner01, owner02, owner03, person04] = await ethers.getSigners();
   console.log("owner01.getAddress():", await owner01.getAddress());
@@ -14,20 +36,37 @@ async function main() {
   console.log("TestLazyMint base uri:", await testLazyMint.baseURI());
 
   owner01Addr = await owner01.getAddress();
+
   tokenId = ethers.BigNumber.from(owner01Addr + "000000000000000000000000");
+
+  const mint1155DataValue = {
+    tokenId: tokenId,
+    uri: "{id}.json",
+    supply: 2,
+    creators: [{ account: owner01Addr, value: 10000 }],
+    royalties: [{ account: owner01Addr, value: 10000 }],
+    signatures: [],
+  };
+
+  signature = await owner01._signTypedData(
+    domain,
+    Mint1155DataType,
+    mint1155DataValue
+  );
 
   await testLazyMint.mintAndTransfer(
     {
       tokenId: tokenId,
       uri: "{id}.json",
       supply: 2,
-      creators: [{ account: owner01.getAddress(), value: 10000 }],
-      royalties: [{ account: owner01.getAddress(), value: 10000 }],
-      signatures: [],
+      creators: [{ account: owner01Addr, value: 10000 }],
+      royalties: [{ account: owner01Addr, value: 10000 }],
+      signatures: [signature],
     },
-    person04.getAddress(),
+    owner01Addr,
     1
   );
+
   console.log("TestLazyMint uri:", await testLazyMint.uri(0));
   console.log(
     "TestLazyMint balance:",
